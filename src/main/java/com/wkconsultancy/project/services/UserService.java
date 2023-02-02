c705b2547fd2448eebba2636c24fce83b36780c9
WeslleySantosln <weslleys.santana299@gmail.com>
2023-02-02 19:18:04 -0300

    Service layer, component registration

---
+++ src/main/java/com/wkconsultancy/project/services/UserService.java
@@ -0,0 +1,28 @@
+package com.wkconsultancy.project.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.wkconsultancy.project.entities.User;
+import com.wkconsultancy.project.repositories.UserRepository;
+
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+		
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
