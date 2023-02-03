cede9307ad48b5d9072f9584cf1b7cd35e002712
WeslleySantosln <weslleys.santana299@gmail.com>
2023-02-02 22:09:53 -0300

    Service layer, component registration

--- src/main/java/com/wkconsultancy/project/entities/User.java
+++ src/main/java/com/wkconsultancy/project/entities/User.java
@@ -1,12 +1,17 @@
 package com.wkconsultancy.project.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -20,6 +25,10 @@ public class User implements Serializable {
 	private Long id;
 	private String name, email, phone, password;
 	
+	
+	@JsonIgnore
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 	
 	public User() {
 	
@@ -83,6 +92,10 @@ public class User implements Serializable {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	
+	public List<Order> getOrders() {
+		return orders;
+	}
 
 
 	@Override
@@ -102,11 +115,6 @@ public class User implements Serializable {
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
-	
-	
-	
-	
+
 	
 }
---
+++ src/main/java/com/wkconsultancy/project/repositories/OrderRepository.java
@@ -0,0 +1,12 @@
+package com.wkconsultancy.project.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.wkconsultancy.project.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+	
+	
+	
+	
+}
