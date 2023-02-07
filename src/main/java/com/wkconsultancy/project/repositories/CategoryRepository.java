438483c701d9ae6eab1806ab41d2d917580a5e7e
WeslleySantosln <weslleys.santana299@gmail.com>
2023-02-06 21:03:48 -0300

    Products

---
+++ src/main/java/com/wkconsultancy/project/repositories/CategoryRepository.java
@@ -0,0 +1,12 @@
+package com.wkconsultancy.project.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.wkconsultancy.project.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+	
+	
+	
+	
+}
---
+++ src/main/java/com/wkconsultancy/project/repositories/ProductRepository.java
@@ -0,0 +1,12 @@
+package com.wkconsultancy.project.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.wkconsultancy.project.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+	
+	
+	
+	
+}
